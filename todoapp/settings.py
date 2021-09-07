1da701948907b91bcc512f1131446da762baae18
Bijay subedi <bijaysubedi33@outlook.com>
2021-09-07 17:35:14 +1000

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -132,15 +132,17 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, '../staticfiles'))
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
 # Extra places for collectstatic to find static files.
 
