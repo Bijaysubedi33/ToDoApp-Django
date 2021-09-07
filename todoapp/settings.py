14361e24714893da45c0c557c448393b8900d904
Bijay Subedi <bijaysubedi33@outlook.com>
2021-09-07 17:24:29 +1000

    Added file

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -131,7 +131,6 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
