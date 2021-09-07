71b6b5aaf7f76d76a3f0c4f9b1378ecffce16526
Bijay subedi <bijaysubedi33@outlook.com>
2021-09-07 17:36:14 +1000

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -9,15 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from os import path
 import os
-import django_heroku
-
 from pathlib import Path
+import djoser
+import django_heroku
+import dj_database_url
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
